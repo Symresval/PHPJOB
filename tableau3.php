9e954bc2649dddc0b87665bda6ffcee4edb02245
Samy ICHALALENE <axelichalalene@gmail.com>
2023-01-24 15:15:33 +0100

    Third commit

--- tableau3.php
+++ tableau3.php
@@ -1,7 +1,6 @@
 <?php
-$marque = array("volvo","BMW","Toyota", "Peugeot","Renault");
+$marque = array("volvo","BMW","Toyota", "Peugeot","Renault",);
 $i=0;
 foreach ($elem as $marque) {
-	echo "Vente de : " . $elem . "<br/>";
+echo "Vente de : " . $elem . "<br/>";
 }
-?>
