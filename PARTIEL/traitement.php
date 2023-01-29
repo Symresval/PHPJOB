5b4cc27b8f0552f0e412f4fc9ac5f3a442106b4d
Symresval2 <123875319+Symresval2@users.noreply.github.com>
2023-01-29 13:56:39 +0100

    Update traitement.php

--- PARTIEL/traitement.php
+++ PARTIEL/traitement.php
@@ -1,17 +1,5 @@
 <?php
-
-require "ressources.php";
-
-$developpeur = array() ;
- 
-$manager = array() ;
-
-
-foreach ($developpeur as $value) {
-	echo $value->afficher();
-}
-
-echo "<br/><br/><br/>";
-foreach ($manager as $value) {
-	echo $value->afficher();
-}
+$developpeur = array(new Developpeur(550,"Le Flock", "Louis-Marie","lmleflock@test.net", "0606060606",2500, "Java"),
+new Developpeur(650,"Chellal", "Amine","ach@test.net", "0707070707",2750, "C++"),
+new Developpeur(151,"Katsiev", "Amir","amir@test.net", "0606060606",2750, "Kotlin")) ;
+$manager = array(new Manager(500,"RIPAULT", "Alexis", "alexis@manager.net","0606060606",4000, "DSI")) ;
